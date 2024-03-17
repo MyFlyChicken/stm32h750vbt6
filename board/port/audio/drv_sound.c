29dc447b7b8e5f2e4ec3942769787f79c6012970
MyFlyChicken <1781202986@qq.com>
2024-03-17 10:49:49 +0800

    可以播放MP3

--- board/port/audio/drv_sound.c
+++ board/port/audio/drv_sound.c
@@ -1,26 +1,20 @@
-/*
- * Copyright (c) 2006-2021, RT-Thread Development Team
- *
- * SPDX-License-Identifier: Apache-2.0
- *
- * Change Logs:
- * Date           Author         Notes
- * 2019-07-28     Ernest         the first version
- */
-
 #include "board.h"
-#include "drv_wm8978.h"
+#include "drv_common.h"
 #include "drv_sound.h"
+#include "stm32h7xx_hal_def.h"
+#include "stm32h7xx_hal_dma.h"
+#include "stm32h7xx_hal_rcc.h"
+#include "stm32h7xx_hal_sai.h"
 
 #define DBG_TAG "drv.sound"
-#define DBG_LVL DBG_INFO
+#define DBG_LVL DBG_LOG
 #include <rtdbg.h>
 
 #define CODEC_I2C_NAME ("i2c1")
 
 #define TX_DMA_FIFO_SIZE (2048)
 
-struct drv_sai _sai_a = {0};
+struct drv_sai _sai3_b = {0};
 
 struct stm32_audio
 {
@@ -33,16 +27,25 @@ struct stm32_audio
 };
 struct stm32_audio _stm32_audio_play = {0};
 
-/* sample_rate, PLLI2SN(50.7), PLLI2SQ, PLLI2SDivQ, MCKDIV */
+/**
+ * @brief 
+ * 
+ * @details 参数分别对应sample_rate, PLL3M PLL3N PLL3P MCKDIV
+ *          PLL3M=25 将PLL3的输入始终分频为25/25=1
+ *          PLL3N    将PLL3输入分频后再进行倍频
+ *          PLL3P    SAI3_B时钟源为PLL3P，这里设置PLL3P
+ *          MCKDIV   samplerate = SAI时钟源 / (MCKDIV * 256 *(OSR+1))根据这个公式可以计算出对应值
+ */
 const rt_uint32_t SAI_PSC_TBL[][5] =
     {
-        {AUDIO_FREQUENCY_048K, 206, 7,  0, 12},
-        {AUDIO_FREQUENCY_044K, 257, 2, 18,  2},
-        {AUDIO_FREQUENCY_032K, 206, 7,  0,  6},
-        {AUDIO_FREQUENCY_022K, 257, 2, 18,  1},
-        {AUDIO_FREQUENCY_016K, 206, 7,  0,  3},
-        {AUDIO_FREQUENCY_011K, 257, 2, 18,  0},
-        {AUDIO_FREQUENCY_008K, 206, 7,  0,  2},
+        {AUDIO_FREQUENCY_008K, 25, 204, 10, 10},
+        {AUDIO_FREQUENCY_011K, 25, 2, 52},
+        {AUDIO_FREQUENCY_016K, 25, 38, 2},
+        {AUDIO_FREQUENCY_032K, 25, 1, 52},
+        {AUDIO_FREQUENCY_044K, 25, 226, 2, 10},
+        {AUDIO_FREQUENCY_048K, 25, 246, 2, 10},
+        {AUDIO_FREQUENCY_096K, 25, 295, 4, 3},
+        {AUDIO_FREQUENCY_192K, 25, 295, 2, 3},
 };
 
 void SAIA_samplerate_set(rt_uint32_t freq)
@@ -62,34 +65,40 @@ void SAIA_samplerate_set(rt_uint32_t freq)
         return;
     }
 
-    PeriphClkInitStruct.PeriphClockSelection = RCC_SAI23CLKSOURCE_PLL3;
-    PeriphClkInitStruct.PLL3.PLL3P           = SAI_PSC_TBL[i][1];
-    //PeriphClkInitStruct.PLLI2S.PLLI2SQ = SAI_PSC_TBL[i][2];
-    //PeriphClkInitStruct.PLLI2SDivQ = SAI_PSC_TBL[i][3] + 1;
+    PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_SAI3;
+    PeriphClkInitStruct.PLL3.PLL3M           = SAI_PSC_TBL[i][1];
+    PeriphClkInitStruct.PLL3.PLL3N           = SAI_PSC_TBL[i][2];
+    PeriphClkInitStruct.PLL3.PLL3P           = SAI_PSC_TBL[i][3];
+    PeriphClkInitStruct.PLL3.PLL3Q           = 2;
+    PeriphClkInitStruct.PLL3.PLL3R           = 2;
+    PeriphClkInitStruct.PLL3.PLL3RGE         = RCC_PLL3VCIRANGE_2;
+    PeriphClkInitStruct.PLL3.PLL3VCOSEL      = RCC_PLL3VCOWIDE;
+    PeriphClkInitStruct.PLL3.PLL3FRACN       = 0;
+    PeriphClkInitStruct.Sai23ClockSelection  = RCC_SAI23CLKSOURCE_PLL3;
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
+    {
+        Error_Handler();
+    }
 
-    HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct);
-
-    //__HAL_RCC_SAI_BLOCKACLKSOURCE_CONFIG(RCC_SAIACLKSOURCE_PLLI2S);
-
-    __HAL_SAI_DISABLE(&_sai_a.hsai);
-    _sai_a.hsai.Init.AudioFrequency = freq;
-    HAL_SAI_Init(&_sai_a.hsai);
-    __HAL_SAI_ENABLE(&_sai_a.hsai);
+    __HAL_SAI_DISABLE(&_sai3_b.hsai);
+    _sai3_b.hsai.Init.AudioFrequency = freq;
+    HAL_SAI_Init(&_sai3_b.hsai);
+    __HAL_SAI_ENABLE(&_sai3_b.hsai);
 }
 
 void SAIA_channels_set(rt_uint16_t channels)
 {
     if (channels == 2)
     {
-        _sai_a.hsai.Init.MonoStereoMode = SAI_STEREOMODE;
+        _sai3_b.hsai.Init.MonoStereoMode = SAI_STEREOMODE;
     }
     else
     {
-        _sai_a.hsai.Init.MonoStereoMode = SAI_MONOMODE;
+        _sai3_b.hsai.Init.MonoStereoMode = SAI_MONOMODE;
     }
-    __HAL_SAI_DISABLE(&_sai_a.hsai);
-    HAL_SAI_Init(&_sai_a.hsai);
-    __HAL_SAI_ENABLE(&_sai_a.hsai);
+    __HAL_SAI_DISABLE(&_sai3_b.hsai);
+    HAL_SAI_Init(&_sai3_b.hsai);
+    __HAL_SAI_ENABLE(&_sai3_b.hsai);
 }
 
 void SAIA_samplebits_set(rt_uint16_t samplebits)
@@ -97,21 +106,21 @@ void SAIA_samplebits_set(rt_uint16_t samplebits)
     switch (samplebits)
     {
     case 16:
-        _sai_a.hsai.Init.DataSize = SAI_DATASIZE_16;
+        _sai3_b.hsai.Init.DataSize = SAI_DATASIZE_16;
         break;
     case 24:
-        _sai_a.hsai.Init.DataSize = SAI_DATASIZE_24;
+        _sai3_b.hsai.Init.DataSize = SAI_DATASIZE_24;
         break;
     case 32:
-        _sai_a.hsai.Init.DataSize = SAI_DATASIZE_32;
+        _sai3_b.hsai.Init.DataSize = SAI_DATASIZE_32;
         break;
     default:
-        _sai_a.hsai.Init.DataSize = SAI_DATASIZE_16;
+        _sai3_b.hsai.Init.DataSize = SAI_DATASIZE_16;
         break;
     }
-    __HAL_SAI_DISABLE(&_sai_a.hsai);
-    HAL_SAI_Init(&_sai_a.hsai);
-    __HAL_SAI_ENABLE(&_sai_a.hsai);
+    __HAL_SAI_DISABLE(&_sai3_b.hsai);
+    HAL_SAI_Init(&_sai3_b.hsai);
+    __HAL_SAI_ENABLE(&_sai3_b.hsai);
 }
 
 void SAIA_config_set(struct rt_audio_configure config)
@@ -121,79 +130,122 @@ void SAIA_config_set(struct rt_audio_configure config)
     SAIA_samplebits_set(config.samplebits);
 }
 
+rt_err_t SAI3B_pin_init(void)
+{
+    GPIO_InitTypeDef GPIO_Initure;
+
+    /* Enable SAI3 clock */
+    __HAL_RCC_SAI3_CLK_ENABLE();
+
+    /* Configure GPIOs used for SAI1 */
+    __HAL_RCC_GPIOD_CLK_ENABLE();
+
+    /**SAI3_B_Block_B GPIO Configuration
+    PD8     ------> SAI3_SCK_B
+    PD9     ------> SAI3_SD_B
+    PD10     ------> SAI3_FS_B
+    PD14     ------> SAI3_MCLK_B
+    */
+    GPIO_Initure.Pin       = GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_14;
+    GPIO_Initure.Mode      = GPIO_MODE_AF_PP;
+    GPIO_Initure.Pull      = GPIO_PULLUP;
+    GPIO_Initure.Speed     = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_Initure.Alternate = GPIO_AF10_SAI2;
+
+    HAL_GPIO_Init(GPIOI, &GPIO_Initure);
+
+    return RT_EOK;
+}
+
 /* initial sai A */
-rt_err_t SAIA_config_init(void)
+rt_err_t SAI3B_config_init(void)
 {
-    SAI_CLK_ENABLE();
+    /* Initialize SAI */
+    __HAL_SAI_RESET_HANDLE_STATE(&_sai3_b.hsai);
 
-    _sai_a.hsai.Instance = SAI1_Block_B;
+    _sai3_b.hsai.Instance = SAI3_Block_B;
 
-    _sai_a.hsai.Init.AudioMode      = SAI_MODEMASTER_TX;        //配置为发送模式
-    _sai_a.hsai.Init.Synchro        = SAI_ASYNCHRONOUS;         //模块内部为异步
-    _sai_a.hsai.Init.OutputDrive    = SAI_OUTPUTDRIVE_ENABLE;   //立刻输出
-    _sai_a.hsai.Init.NoDivider      = SAI_MASTERDIVIDER_ENABLE; //使能MCK输出NOACK
-    _sai_a.hsai.Init.FIFOThreshold  = SAI_FIFOTHRESHOLD_1QF;    //1/4FIFO
-    _sai_a.hsai.Init.MonoStereoMode = SAI_STEREOMODE;
-    _sai_a.hsai.Init.AudioFrequency = _usAudioFreq;
-    _sai_a.hsai.Init.Protocol       = SAI_FREE_PROTOCOL;            //设置SAI1协议为:自由协议(支持I2S/LSB/MSB/TDM/PCM/DSP等协议)
-    _sai_a.hsai.Init.DataSize       = SAI_DATASIZE_16;              //设置数据大小
-    _sai_a.hsai.Init.FirstBit       = SAI_FIRSTBIT_MSB;             //数据MSB位优先
-    _sai_a.hsai.Init.ClockStrobing  = SAI_CLOCKSTROBING_RISINGEDGE; //数据在时钟的上升/下降沿选通
+    __HAL_SAI_DISABLE(&_sai3_b.hsai);
 
-    //帧设置
-    _sai_a.hsai.FrameInit.FrameLength       = 32;
-    _sai_a.hsai.FrameInit.ActiveFrameLength = 16;
-    _sai_a.hsai.FrameInit.FSDefinition      = SAI_FS_CHANNEL_IDENTIFICATION; //FS信号为SOF信号+通道识别信号
-    _sai_a.hsai.FrameInit.FSPolarity        = SAI_FS_ACTIVE_LOW;             //FS低电平有效(下降沿)
-    _sai_a.hsai.FrameInit.FSOffset          = SAI_FS_BEFOREFIRSTBIT;         //在slot0的第一位的前一位使能FS,以匹配飞利浦标准
+    _sai3_b.hsai.Init.AudioMode      = SAI_MODEMASTER_TX;
+    _sai3_b.hsai.Init.Synchro        = SAI_ASYNCHRONOUS;
+    _sai3_b.hsai.Init.OutputDrive    = SAI_OUTPUTDRIVE_ENABLE;
+    _sai3_b.hsai.Init.NoDivider      = SAI_MASTERDIVIDER_ENABLE;
+    _sai3_b.hsai.Init.FIFOThreshold  = SAI_FIFOTHRESHOLD_1QF;
+    _sai3_b.hsai.Init.AudioFrequency = SAI_AUDIO_FREQUENCY_48K;
+    _sai3_b.hsai.Init.Protocol       = SAI_FREE_PROTOCOL;
+    _sai3_b.hsai.Init.DataSize       = SAI_DATASIZE_16;
+    _sai3_b.hsai.Init.FirstBit       = SAI_FIRSTBIT_MSB;
+    _sai3_b.hsai.Init.ClockStrobing  = SAI_CLOCKSTROBING_FALLINGEDGE;
 
-    //SLOT设置
-    _sai_a.hsai.SlotInit.FirstBitOffset = 0;                                   //slot偏移(FBOFF)为0
-    _sai_a.hsai.SlotInit.SlotSize       = SAI_SLOTSIZE_16B;                    //slot大小为32位
-    _sai_a.hsai.SlotInit.SlotNumber     = 2;                                   //slot数为2个
-    _sai_a.hsai.SlotInit.SlotActive     = SAI_SLOTACTIVE_0 | SAI_SLOTACTIVE_1; //使能slot0和slot1
+    //TODO frame 必须将FrameLength设置为64 ActiveFrameLength设置为32 否则会有很大的底噪。具体原因尚不明确
+    _sai3_b.hsai.FrameInit.FrameLength       = 64;
+    _sai3_b.hsai.FrameInit.ActiveFrameLength = 32;
+    _sai3_b.hsai.FrameInit.FSDefinition      = SAI_FS_CHANNEL_IDENTIFICATION;
+    _sai3_b.hsai.FrameInit.FSPolarity        = SAI_FS_ACTIVE_LOW;
+    _sai3_b.hsai.FrameInit.FSOffset          = SAI_FS_BEFOREFIRSTBIT;
 
-    HAL_SAI_Init(&_sai_a.hsai); //初始化SAI
-    //  HAL_SAI_InitProtocol(&h_sai, SAI_I2S_STANDARD, _usWordLen, 2);//2--left_channel and right_channel
-    SAI1_Block_B->CR1 |= 1 << 17; //写入CR1寄存器中
-    __HAL_SAI_ENABLE(&_sai_a.hsai);
+    //slot
+    _sai3_b.hsai.SlotInit.FirstBitOffset = 0;
+    _sai3_b.hsai.SlotInit.SlotSize       = SAI_SLOTSIZE_32B;
+    _sai3_b.hsai.SlotInit.SlotNumber     = 2;
+    _sai3_b.hsai.SlotInit.SlotActive     = (SAI_SLOTACTIVE_0 | SAI_SLOTACTIVE_1);
+
+    if (HAL_OK != HAL_SAI_Init(&_sai3_b.hsai))
+    {
+        Error_Handler();
+    };
+    __HAL_SAI_ENABLE(&_sai3_b.hsai);
 
     return RT_EOK;
 }
 
-rt_err_t SAIA_tx_dma(void)
+rt_err_t SAI3B_tx_dma(void)
 {
-    DMA_CLK_ENABLE();
+    __HAL_RCC_DMA2_CLK_ENABLE();
+    __HAL_LINKDMA(&_sai3_b.hsai, hdmatx, _sai3_b.hdma);
 
-    _sai_a.hdma.Instance                 = DMA1_Stream2;
-    _sai_a.hdma.Init.Request             = DMA_REQUEST_SAI1_B;
-    _sai_a.hdma.Init.Direction           = DMA_MEMORY_TO_PERIPH;
-    _sai_a.hdma.Init.PeriphInc           = DMA_PINC_DISABLE;
-    _sai_a.hdma.Init.MemInc              = DMA_MINC_ENABLE;
-    _sai_a.hdma.Init.Mode                = DMA_CIRCULAR;
-    _sai_a.hdma.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
-    _sai_a.hdma.Init.MemDataAlignment    = DMA_MDATAALIGN_HALFWORD;
-    _sai_a.hdma.Init.FIFOMode            = DMA_FIFOMODE_DISABLE;
-    _sai_a.hdma.Init.Priority            = DMA_PRIORITY_HIGH;
-    _sai_a.hdma.Init.MemBurst            = DMA_MBURST_SINGLE; //存储器单次突发传输
-    _sai_a.hdma.Init.PeriphBurst         = DMA_PBURST_SINGLE; //外设突发单次传输
+    /* Select the DMA instance to be used for the transfer : DMA2_Stream3 */
+    _sai3_b.hdma.Instance = DMA2_Stream3;
 
-    __HAL_LINKDMA(&_sai_a.hdma, hdmatx, _sai_a.hdma); //将DMA与SAI联系起来
-    HAL_DMA_Init(&_sai_a.hdma);                       //初始化DMA
+    _sai3_b.hdma.Init.Request   = DMA_REQUEST_SAI3_B;
+    _sai3_b.hdma.Init.Direction = DMA_MEMORY_TO_PERIPH;
+    _sai3_b.hdma.Init.PeriphInc = DMA_PINC_DISABLE;
+    _sai3_b.hdma.Init.MemInc    = DMA_MINC_ENABLE;
 
-    __HAL_DMA_ENABLE_IT(&_sai_a.hdma, DMA_IT_TC);
-    HAL_DMAEx_MultiBufferStart(&_sai_a.hdma, (uint32_t)buffer0, (uint32_t)&SAI1_Block_B->DR, (uint32_t)buffer1, num); //开启双缓冲
+    _sai3_b.hdma.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
+    _sai3_b.hdma.Init.MemDataAlignment    = DMA_MDATAALIGN_HALFWORD;
 
-    HAL_NVIC_SetPriority(DMA1_Stream2_IRQn, 0, 0); //DMA中断优先级
-    HAL_NVIC_EnableIRQ(DMA1_Stream2_IRQn);
+    _sai3_b.hdma.Init.Mode          = DMA_CIRCULAR;
+    _sai3_b.hdma.Init.Priority      = DMA_PRIORITY_HIGH;
+    _sai3_b.hdma.Init.FIFOMode      = DMA_FIFOMODE_ENABLE;
+    _sai3_b.hdma.Init.FIFOThreshold = DMA_FIFO_THRESHOLD_FULL;
+    _sai3_b.hdma.Init.MemBurst      = DMA_MBURST_SINGLE;
+    _sai3_b.hdma.Init.PeriphBurst   = DMA_PBURST_SINGLE;
 
+    /* Deinitialize the Stream for new transfer */
+    HAL_DMA_DeInit(&_sai3_b.hdma);
+
+    /* Configure the DMA Stream */
+    if (HAL_OK != HAL_DMA_Init(&_sai3_b.hdma))
+    {
+        Error_Handler();
+    }
+
+    __HAL_DMA_DISABLE(&_sai3_b.hdma);
+
+    __HAL_DMA_ENABLE_IT(&_sai3_b.hdma, DMA_IT_TC);
+    __HAL_DMA_CLEAR_FLAG(&_sai3_b.hdma, DMA_FLAG_TCIF3_7);
+    /* set nvic */
+    HAL_NVIC_SetPriority(DMA2_Stream3_IRQn, 5, 0);
+    HAL_NVIC_EnableIRQ(DMA2_Stream3_IRQn);
     return RT_EOK;
 }
 
-void DMA1_Stream2_IRQHandler(void)
+void DMA2_Stream3_IRQHandler(void)
 {
     rt_interrupt_enter();
-    HAL_DMA_IRQHandler(_sai_a.hsai.hdmatx);
+    HAL_DMA_IRQHandler(_sai3_b.hsai.hdmatx);
     rt_interrupt_leave();
 }
 
@@ -207,11 +259,11 @@ void HAL_SAI_TxCpltCallback(SAI_HandleTypeDef* hsai)
     rt_audio_tx_complete(&_stm32_audio_play.audio);
 }
 
-rt_err_t sai_a_init()
+rt_err_t sai_init(void)
 {
-    /* set sai_a DMA */
-    SAIA_tx_dma();
-    SAIA_config_init();
+    SAI3B_pin_init();
+    SAI3B_tx_dma();
+    SAI3B_config_init();
 
     return RT_EOK;
 }
@@ -316,7 +368,7 @@ static rt_err_t stm32_player_configure(struct rt_audio_device* audio, struct rt_
         {
         case AUDIO_MIXER_MUTE: {
             /* set mute mode */
-            wm8978_mute_enabled(_stm32_audio_play.i2c_bus, RT_FALSE);
+            //wm8978_mute_enabled(_stm32_audio_play.i2c_bus, RT_FALSE);
             break;
         }
 
@@ -325,7 +377,7 @@ static rt_err_t stm32_player_configure(struct rt_audio_device* audio, struct rt_
 
             st_audio->replay_volume = volume;
             /* set mixer volume */
-            wm8978_set_volume(_stm32_audio_play.i2c_bus, volume);
+            //wm8978_set_volume(_stm32_audio_play.i2c_bus, volume);
 
             break;
         }
@@ -387,10 +439,10 @@ static rt_err_t stm32_player_configure(struct rt_audio_device* audio, struct rt_
 static rt_err_t stm32_player_init(struct rt_audio_device* audio)
 {
     /* initialize wm8978 */
-    _stm32_audio_play.i2c_bus = (struct rt_i2c_bus_device*)rt_device_find(CODEC_I2C_NAME);
+    //_stm32_audio_play.i2c_bus = (struct rt_i2c_bus_device*)rt_device_find(CODEC_I2C_NAME);
 
-    sai_a_init();
-    wm8978_init(_stm32_audio_play.i2c_bus);
+    sai_init();
+    //wm8978_init(_stm32_audio_play.i2c_bus);
     return RT_EOK;
 }
 
@@ -398,8 +450,8 @@ static rt_err_t stm32_player_start(struct rt_audio_device* audio, int stream)
 {
     if (stream == AUDIO_STREAM_REPLAY)
     {
-        HAL_SAI_Transmit_DMA(&_sai_a.hsai, _stm32_audio_play.tx_fifo, TX_DMA_FIFO_SIZE / 2);
-        wm8978_player_start(_stm32_audio_play.i2c_bus);
+        HAL_SAI_Transmit_DMA(&_sai3_b.hsai, _stm32_audio_play.tx_fifo, TX_DMA_FIFO_SIZE / 2);
+        //wm8978_player_start(_stm32_audio_play.i2c_bus);
     }
 
     return RT_EOK;
@@ -409,7 +461,7 @@ static rt_err_t stm32_player_stop(struct rt_audio_device* audio, int stream)
 {
     if (stream == AUDIO_STREAM_REPLAY)
     {
-        HAL_SAI_DMAStop(&_sai_a.hsai);
+        HAL_SAI_DMAStop(&_sai3_b.hsai);
     }
 
     return RT_EOK;
@@ -442,14 +494,29 @@ static struct rt_audio_ops _p_audio_ops =
 
 int rt_hw_sound_init(void)
 {
-    rt_uint8_t* tx_fifo;
-
+    rt_uint8_t*        tx_fifo;
+    struct rt_memheap* memheap;
+#if 1
     /* player */
-    tx_fifo = rt_malloc(TX_DMA_FIFO_SIZE);
-    if (tx_fifo == RT_NULL)
+    memheap = sys_memheap_get(MEMHEAP_AXI_SRAM);
+    if (memheap != RT_NULL)
+    {
+        tx_fifo = rt_memheap_alloc(memheap, TX_DMA_FIFO_SIZE);
+        if (tx_fifo == RT_NULL)
+        {
+            return -RT_ENOMEM;
+        }
+    }
+    else
     {
         return -RT_ENOMEM;
     }
+
+#else
+    rt_align(4) static rt_uint8_t buffer[2048] __attribute__((at(0x24001000)));
+    tx_fifo = buffer;
+#endif
+
     rt_memset(tx_fifo, 0, TX_DMA_FIFO_SIZE);
     _stm32_audio_play.tx_fifo = tx_fifo;
 
@@ -459,5 +526,4 @@ int rt_hw_sound_init(void)
 
     return RT_EOK;
 }
-
 INIT_DEVICE_EXPORT(rt_hw_sound_init);
